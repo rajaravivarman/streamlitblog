20b2598de49154e808a9badb8b31f1d3670b3f3d
Raja Ravi Varman <email@rajaravivarman.com>
2023-12-08 12:20:13 +0530

    Added initial code

---
+++ README.md
@@ -0,0 +1,31 @@
+Here is a possible read me for the blog app you created:
+
+# Blog App: A simple and elegant blog app built with Streamlit and Python
+
+Blog App is a web application that allows you to create, view, search, and manage blog posts using Streamlit and Python¹[1]. Blog App uses SQLite as the database to store the posts and their details. Blog App also provides some statistics and visualizations of the posts and authors.
+
+## Installation
+
+To run Blog App, you need to have Python 3.6 or higher installed on your system. You also need to install Streamlit and its dependencies using the following command:
+
+pip install streamlit
+
+You can also use a virtual environment to isolate the dependencies. For more details, you can refer to the web search results I found for you .
+
+## Usage
+
+To run Blog App, you need to use the streamlit run command in your command prompt or Anaconda shell. For example, if your file name is blog_app.py, you can type:
+
+streamlit run blog_app.py
+
+This will start a local Streamlit server and open your app in a new tab in your default web browser. You can also pass arguments to your file or use a URL instead of a file name. For more details, you can refer to the web search results I found for you .
+
+## Features
+
+Blog App has a sidebar menu with different options:
+
+- **Home**: This option displays the welcome message and some information about the app.
+- **View Posts**: This option displays all the posts in the blog. You can click on the "Read More" button to view the full post. You can also sort the posts by date or title using the dropdown menu.
+- **Add Post**: This option allows you to add a new post to the blog. You need to fill in the author, title, content, and date fields and click on the "Submit" button. You will see a success message if the post is added successfully.
+- **Search**: This option allows you to search for a post by title or author. You need to enter your query in the text input and press enter. You will see the matching posts if any, or a message if none are found.
+- **Manage**: This option allows you to delete posts or view some statistics. You need to select a post to delete from the dropdown menu and click on the "Delete" button. You will see a confirmation message if the post is deleted successfully. You can also check the "Show statistics" box to see some basic statistics and a bar chart of posts by author.
---
+++ blog.py
@@ -0,0 +1,146 @@
+# Import streamlit and other libraries
+import streamlit as st
+import sqlite3
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Connect to the database
+conn = sqlite3.connect('blog.db')
+c = conn.cursor()
+
+# Create a table if not exists
+c.execute('CREATE TABLE IF NOT EXISTS posts (author TEXT, title TEXT, content TEXT, date DATE)')
+
+# Define some functions for interacting with the database
+def add_post(author, title, content, date):
+    c.execute('INSERT INTO posts (author, title, content, date) VALUES (?,?,?,?)', (author, title, content, date))
+    conn.commit()
+
+def get_all_posts():
+    c.execute('SELECT * FROM posts')
+    data = c.fetchall()
+    return data
+
+def get_post_by_title(title):
+    c.execute('SELECT * FROM posts WHERE title=?', (title,))
+    data = c.fetchone()
+    return data
+
+def delete_post(title):
+    c.execute('DELETE FROM posts WHERE title=?', (title,))
+    conn.commit()
+
+# Define some HTML templates for displaying the posts
+title_temp = """
+<div style="background-color:#464e5f;padding:10px;border-radius:10px;margin:10px;">
+<h4 style="color:white;text-align:center;">{}</h4>
+<img src="https://www.w3schools.com/howto/img_avatar.png" alt="Avatar" style="vertical-align: middle;float:left;width: 50px;height: 50px;border-radius: 50%;">
+<h6>Author: {}</h6>
+<br/>
+<br/>
+<p style="text-align:justify"> {}</p>
+</div>
+"""
+
+post_temp = """
+<div style="background-color:#464e5f;padding:10px;border-radius:5px;margin:10px;">
+<h4 style="color:white;text-align:center;">{}</h4>
+<h6>Author: {}</h6>
+<h6>Date: {}</h6>
+<img src="https://www.w3schools.com/howto/img_avatar.png" alt="Avatar" style="vertical-align: middle;width: 50px;height: 50px;border-radius: 50%;">
+<br/>
+<br/>
+<p style="text-align:justify"> {}</p>
+</div>
+"""
+
+# Create a sidebar menu with different options
+menu = ["Home", "View Posts", "Add Post", "Search", "Manage"]
+choice = st.sidebar.selectbox("Menu", menu)
+
+# Display the selected option
+if choice == "Home":
+    st.title("Welcome to my blog")
+    st.write("This is a simple blog app built with streamlit and python.")
+    st.write("You can view, add, search, and manage posts using the sidebar menu.")
+    st.write("Enjoy!")
+
+elif choice == "View Posts":
+    st.title("View Posts")
+    st.write("Here you can see all the posts in the blog.")
+    # Get all the posts from the database
+    posts = get_all_posts()
+    # Display each post as a card
+    for post in posts:
+        st.markdown(title_temp.format(post[1], post[0], post[2][:50] + "..."), unsafe_allow_html=True)
+        # Add a button to view the full post
+        if st.button("Read More", key=post[1]):
+            st.markdown(post_temp.format(post[1], post[0], post[3], post[2]), unsafe_allow_html=True)
+
+elif choice == "Add Post":
+    st.title("Add Post")
+    st.write("Here you can add a new post to the blog.")
+    # Create a form to get the post details
+    with st.form(key="add_form"):
+        author = st.text_input("Author")
+        title = st.text_input("Title")
+        content = st.text_area("Content")
+        date = st.date_input("Date")
+        submit = st.form_submit_button("Submit")
+    # If the form is submitted, add the post to the database
+    if submit:
+        add_post(author, title, content, date)
+        st.success("Post added successfully")
+
+elif choice == "Search":
+    st.title("Search")
+    st.write("Here you can search for a post by title or author.")
+    # Create a text input to get the search query
+    query = st.text_input("Enter your query")
+    # If the query is not empty, search for the matching posts
+    if query:
+        # Get all the posts from the database
+        posts = get_all_posts()
+        # Filter the posts by the query
+        results = [post for post in posts if query.lower() in post[0].lower() or query.lower() in post[1].lower()]
+        # Display the results
+        if results:
+            st.write(f"Found {len(results)} matching posts:")
+            for result in results:
+                st.markdown(title_temp.format(result[1], result[0], result[2][:50] + "..."), unsafe_allow_html=True)
+                # Add a button to view the full post
+                if st.button("Read More", key=result[1]):
+                    st.markdown(post_temp.format(result[1], result[0], result[3], result[2]), unsafe_allow_html=True)
+        else:
+            st.write("No matching posts found")
+
+elif choice == "Manage":
+    st.title("Manage")
+    st.write("Here you can delete posts or view some statistics.")
+    # Create a selectbox to choose a post to delete
+    titles = [post[1] for post in get_all_posts()]
+    title = st.selectbox("Select a post to delete", titles)
+    # Add a button to confirm the deletion
+    if st.button("Delete"):
+        delete_post(title)
+        st.success("Post deleted successfully")
+    # Create a checkbox to show some statistics
+    if st.checkbox("Show statistics"):
+        # Get all the posts from the database
+        posts = get_all_posts()
+        # Convert the posts to a dataframe
+        df = pd.DataFrame(posts, columns=["author", "title", "content", "date"])
+        # Display some basic statistics
+        st.write("Number of posts:", len(posts))
+        st.write("Number of authors:", len(df["author"].unique()))
+        st.write("Most recent post:", df["date"].max())
+        st.write("Oldest post:", df["date"].min())
+        # Display a bar chart of posts by author
+        st.write("Posts by author:")
+        author_count = df["author"].value_counts()
+        fig, ax = plt.subplots()
+        ax.bar(author_count.index, author_count.values)
+        ax.set_xlabel("Author")
+        ax.set_ylabel("Number of posts")
+        ax.set_title("Posts by author")
+        st.pyplot(fig)
